730ba0530c97d0d7b844ea8c1121893fe5903bdc
nandirajupriyanka <priyankanandiraju@Priyankas-MacBook-Pro-2.local>
2017-06-26 23:52:05 -0500

    feat: Create a Java library that provides joke
    Create a new Java joke telling library for the project.
    Make the button display a toast showing a joke retrieved from the
    library

---
+++ jokeslibrary/src/main/java/com/example/TellJoke.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class TellJoke {
+    public String getJoke() {
+        return "My friend thinks he is smart. He told me an onion is the only food that makes you cry, so I threw a coconut at his face.";
+    }
+}
