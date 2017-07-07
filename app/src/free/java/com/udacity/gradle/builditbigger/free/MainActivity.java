f942c9efe1c8004bf334853c7fdb2de682e0156d
nandirajupriyanka <priyankanandiraju@Priyankas-MacBook-Pro-2.local>
2017-07-06 19:53:13 -0500

    feat: Add an interstitial ad to the free version
    - Display the interstitial ad after the user hits the button, and
    before the joke is shown for the free version
    - Reformat code by creating new class for Async task since it is common
    in all flavors.

---
+++ app/src/free/java/com/udacity/gradle/builditbigger/free/MainActivity.java
@@ -0,0 +1,78 @@
+package com.udacity.gradle.builditbigger.free;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.util.Pair;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+import com.udacity.gradle.builditbigger.EndPointsAsyncTask;
+import com.udacity.gradle.builditbigger.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private InterstitialAd mInterstitialAd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+        loadNewInterstitialAd();
+    }
+
+    private void loadNewInterstitialAd() {
+        mInterstitialAd.loadAd(new AdRequest.Builder().build());
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void tellJoke(View view) {
+        if (mInterstitialAd.isLoaded()) {
+            mInterstitialAd.show();
+        } else {
+            Log.d(TAG, "The interstitial wasn't loaded yet.");
+            new EndPointsAsyncTask().execute(new Pair<Context, String>(MainActivity.this, "Hey"));
+        }
+
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                loadNewInterstitialAd();
+                new EndPointsAsyncTask().execute(new Pair<Context, String>(MainActivity.this, "Hey"));
+            }
+        });
+    }
+}
